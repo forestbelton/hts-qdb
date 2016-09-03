ff56b5a419bafe3f610a573a6f7b4c7279444e43
Forest Belton <forest@homolo.gy>
2016-09-02 18:33:55 -0700

    Add static asset middleware

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
+import Network.Wai
 import Network.Wai.Handler.Warp
 import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Middleware.Static
 import Servant
 
 import QDB.API
@@ -9,7 +11,10 @@ import QDB.API
 port :: Int
 port = 8080
 
+middleware :: Application -> Application
+middleware = staticPolicy (addBase "client/static") . logStdout
+
 main :: IO ()
 main = do
     putStrLn $ "Now listening on port " ++ show port ++ "..."
-    run port $ logStdout $ serve qdbAPI qdbServer
+    run port $ middleware $ serve qdbAPI qdbServer
