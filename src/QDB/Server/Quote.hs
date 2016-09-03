26aa30f5d9dfd367f355432b81a9c88cf991ca37
Forest Belton <forest@homolo.gy>
2016-09-02 17:13:41 -0700

    Remove undefined usage in dummyQuote

--- src/QDB/Server/Quote.hs
+++ src/QDB/Server/Quote.hs
@@ -1,26 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
 module QDB.Server.Quote (quoteAPI) where
 
+import Control.Monad.IO.Class
+import Data.Time.Clock
 import Servant
 
 import QDB.API.Quote
 import QDB.Model.Quote
 import QDB.Types
 
-dummyQuote :: Quote
-dummyQuote = Quote (ID 0) undefined 0 0 ""
+dummyQuote :: IO Quote
+dummyQuote = do
+    time <- getCurrentTime
+    return $ Quote (ID 0) time 0 0 ""
 
 getQuote :: Server GetQuote
-getQuote id = return dummyQuote
+getQuote id = liftIO dummyQuote
 
 getQuotes :: Server GetQuotes
 getQuotes sortBy = return []
 
 addQuote :: Server AddQuote
-addQuote = return dummyQuote
+addQuote = liftIO dummyQuote
 
 editQuote :: Server EditQuote
-editQuote id action = return dummyQuote
+editQuote id action = liftIO dummyQuote
 
 quoteAPI :: Server QuoteAPI
 quoteAPI = getQuote
